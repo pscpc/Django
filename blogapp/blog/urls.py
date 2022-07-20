008ff5a173138da2c9e9e698992e32b5edb4335f
Ibrahim <ibrahim.ozay97@gmail.com>
2022-07-21 00:05:40 +0300

    first commit

---
+++ blogapp/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+#http://127.0.0.1:8000/
+
+urlpatterns = [
+    path("", views.index),
+    path("index", views.index),
+    path("blogs", views.blogs),
+    path("blogs/<int:id>", views.blog_details)
+]
